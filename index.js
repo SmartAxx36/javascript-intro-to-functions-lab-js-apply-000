d6e9f0e9fd3fa9fa9561bc5ff58b6f5fd59a1334
Gavin Moore <gavizoid16@gmail.com>
2018-09-03 21:37:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ function sayHiToGrandma(string) {
   if (string === 'hello') {
     return "I can\'t hear you!"
   }
-  if (string === 'HELLO' {
-    
+  if (string === 'HELLO') {
+    return "YES INDEED!"
+  } else {
+    return "I love you, too."
   }
+  return string
 }
